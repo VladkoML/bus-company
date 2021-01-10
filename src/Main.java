13d3acfb10288c3dbed2c8743b2f8fb0e08d8aa8
VladkoML <saintpeople1488@gmail.com>
2021-01-10 13:53:16 +0200

    added main constructor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    public Main() {
+        setTitle("Автобусна компанія");
+        setSize(500, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 
     public static void main(String[] args) {
+
+        Main main = new Main();
 
     }
 
